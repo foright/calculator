bea67176f6e38a94f73690b86265fa695cd88b5a
jason <chenchaoqun@ihuilian.com>
2014-09-26 17:06:32 +0800

    calculator v1

---
+++ calculator.py
@@ -0,0 +1,21 @@
+#!usr/bin/env python
+#--- coding=utf-8 ----
+# 1. 支持四则运算 +-×/% 
+# 2. 支持数论中的全部类型
+# 
+# 方法论：
+# 1.输入值分解，按优先级排列
+# 2.分步计算
+import sys
+
+def scan(str):
+    str.
+
+
+
+def calculator(s):
+    pass
+
+if__name__ == '__main__':
+    s = raw_input(str)
+    calculator(s)
